4ed6359eddd523d889c68690ca84c30380215436
unknown <lakavathsachin.93@gmail.com>
2022-04-13 19:22:45 +0530

    first commit

---
+++ random.js
@@ -0,0 +1,10 @@
+function random(){
+    
+    const randomColor = Math.floor(Math.random()*16777215).toString(16);
+    document.body.style.backgroundColor = "#" + randomColor;
+   
+  
+  genNew.addEventListener("click", random);
+  setBg();
+}
+
